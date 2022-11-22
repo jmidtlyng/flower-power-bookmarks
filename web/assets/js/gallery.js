13313275dcfb9b56876599312c8826ff1b1c023b
yk <jacobmidtlyng@gmail.com>
2022-11-21 23:02:14 -0800

    styling cleanups. imrpove navigation and postioning. next add to cart function

--- web/assets/js/gallery.js
+++ web/assets/js/gallery.js
@@ -1,5 +1,17 @@
 // get reference of all gallery items
-const els_galleryItems = document.getElementsByClassName('gallery-item');
+const el_gallery = document.getElementById('gallery'),
+			els_galleryItems = document.getElementsByClassName('gallery-item'),
+			els_scrollDown = document.getElementsByClassName('gallery-item-page-control--down'),
+			els_scrollUp = document.getElementsByClassName('gallery-item-page-control--up');
+			
+// loop down navigation buttons adding scroll events
+for(const el_scrollDown of els_scrollDown){
+	el_scrollDown.addEventListener('click', scrollToNext);
+}
+// loop up navigation buttons adding scroll events
+for(const el_scrollUp of els_scrollUp){
+	el_scrollUp.addEventListener('click', scrollToPrev);
+}
 
 // loop gallery items setting timeout load of detailed image
 for(const el_galleryItem of els_galleryItems){
@@ -17,11 +29,22 @@ function revealHdPhoto(el_galleryItem) {
 	if(el_itemPhoto !== null){
 		// if drawing exists, hide
 		if(el_itemDrawing !== null){
-			// el_itemDrawing.classList.add('gallery-item-display--invisible');
+			el_itemDrawing.classList.add('gallery-item-display--invisible');
 			el_itemNote.classList.add('gallery-item-display--invisible');
 		}
 		
 		// reveal photo
-		// el_itemPhoto.classList.remove('gallery-item-display--invisible');
+		el_itemPhoto.classList.remove('gallery-item-display--invisible');
 	}
+}
+
+function scrollToNext() {
+	el_gallery.scrollBy({ top: el_gallery.clientHeight,
+												left: 0,
+												behavior: 'smooth' });
+}
+function scrollToPrev() {
+	el_gallery.scrollBy({ top: -el_gallery.clientHeight,
+												left: 0,
+												behavior: 'smooth' });
 }
