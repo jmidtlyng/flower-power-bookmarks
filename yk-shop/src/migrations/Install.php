81e2bbf4beb09f4e37c9652c7f7c4dd9d3545f65
yk <jacobmidtlyng@gmail.com>
2022-12-09 11:17:36 -0800

    try adding fields for stock and price

--- yk-shop/src/migrations/Install.php
+++ yk-shop/src/migrations/Install.php
@@ -4,6 +4,7 @@ namespace YouKnowww\YkShop\migrations;
 use Craft;
 use craft\db\Migration;
 use craft\models\Section;
+use craft\models\Fields;
 
 class Install extends Migration{
 		public function safeUp(): bool {
@@ -26,6 +27,62 @@ class Install extends Migration{
 				
 				Craft::$app->sections->saveSection($section);
 			}
+			
+			// Create the field group
+			$groupModel = new FieldGroupModel();
+			$groupModel->name = 'YkShopProducts';
+			craft()->fields->saveGroup($groupModel);
+			
+			// I haven't yet found a way to get the group ID without looping through all of the groups. saveGroup() returns a boolean and doesn't appear to update the id attribute of the original model instance.
+			$groups = craft()->fields->getAllGroups();
+			foreach($groups as $group) {
+					if($group->name != 'YkShopProducts') {
+							continue;
+					}
+					$groupModel = $group;
+			}
+			
+			// Create the tab
+			$tabModel = new FieldLayoutTabModel();
+			// Get the desired layout (in this case, the User layout) and use the tab's setLayout to link the two
+			$layout = craft()->fields->getLayoutByType(ElementType::Entry);
+			$tabModel->setLayout($layout);
+			$tabModel->name = 'YkShopProducts';
+			
+			
+			// Create the fields. I have a protected class member that contains the fields in a 'fieldName' => 'FieldName' format.
+			$price_field = new FieldModel();
+			$price_field->groupId = $groupModel->id;
+			$price_field->name = Craft::t('Price');
+			$price_field->handle = 'yk_shop_price';
+			$price_field->translatable = false;
+			$price_field->type = 'Number';
+			
+			// quantity field
+			$stock_field = new FieldModel();
+			$stock_field->groupId = $groupModel->id;
+			$stock_field->name = Craft::t('Stock');
+			$stock_field->handle = 'yk_shop_stock';
+			$stock_field->translatable = false;
+			$stock_field->type = 'Number';
+	
+			craft()->fields->saveField($price_field);
+			
+			// Get all of the group's fields. This could also be done by saving an array of the fields in the loop above, but this seems a bit cleaner to me, and since we already have the group object, saves us an object/variable declaration.
+			$fields = $groupModel->getFields();
+			
+			// Set the tab's fields. This gives us a custom grouping in the layout editor.
+			$tabModel->setFields($fields);
+			
+			// Get the existing tabs (and, by extension, fields), since we don't want to nuke the layout, just add to it. I couldn't find any sort of "addTab" type function, so this is how we have to do it.
+			$layoutTabs = $layout->getTabs();
+			
+			// getTabs() returns an array, so simply add our new tab to the array.
+			$layoutTabs[] = $tabModel;
+			
+			// Now, set the tabs back in the layout.
+			$layout->setTabs($layoutTabs);
+			
 			// done approve
 			return true;
 		}
